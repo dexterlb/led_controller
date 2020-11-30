f26e1b41499a5e26d487a705beb7643ee5bff6fe
dexterlb <dexterlb@qtrp.org>
2020-11-30 14:46:09 +0200

    add nodemcu stuff

---
+++ nodemcu/fs/credentials_example.lua
@@ -0,0 +1,12 @@
+SSID = "Tardis"
+PASSWORD = "the_pass"
+
+MQTT_AUTH = false   -- true if we need to use authentication when connecting
+MQTT_TLS = false
+MQTT_HOST = "some_host"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE = 120  -- seconds
+MQTT_USERNAME = "foo"
+MQTT_PASSWORD = "bar"
+POTOO_ROOT = "leddy"
+MQTT_CLIENT_ID = MQTT_ROOT
