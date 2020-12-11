fa9b63159bb281d276f3f31d31dab293e9f00987
dexterlb <dexterlb@qtrp.org>
2020-12-11 12:45:11 +0200

    remove paired channel functionality

--- stm/src/main.c
+++ stm/src/main.c
@@ -14,36 +14,26 @@ void Error_Handler(void);
 
 typedef struct {
     // settable by user:
-    fixedpt gamma_a;
-    fixedpt gamma_b;
+    fixedpt gamma;
     fixedpt val;
-    fixedpt ratio;
     fixedpt fade_speed;
 
     // computed:
-    fixedpt actual_a;
-    fixedpt actual_b;
-
-    fixedpt linear_a;
-    fixedpt linear_b;
-
-    int duty_a;
-    int duty_b;
+    fixedpt actual;
+    fixedpt linear;
+    int duty;
 } chan_t;
 
 typedef struct {
-    chan_t channels[1];
+    chan_t channels[2];
 } state_t;
 
 void default_state(state_t* state) {
     for (size_t i = 0; i < length(state->channels); i++) {
-        state->channels[i].gamma_a = fixedpt_rconst(2.2);
-        state->channels[i].gamma_b = fixedpt_rconst(2.2);
         state->channels[i].val = fixedpt_rconst(0.5);
-        state->channels[i].ratio = fixedpt_rconst(0.5);
         state->channels[i].fade_speed = fixedpt_rconst(2.0);
-        state->channels[i].actual_a = fixedpt_rconst(0.0);
-        state->channels[i].actual_b = fixedpt_rconst(0.0);
+        state->channels[i].gamma = fixedpt_rconst(2.2);
+        state->channels[i].actual = fixedpt_rconst(0.0);
     }
 }
 
@@ -51,22 +41,19 @@ chan_t* resolve_chan(state_t* state, uint8_t* chan_name) {
     if (string_eq(chan_name, str("ch1"))) {
         return &state->channels[0];
     }
+    if (string_eq(chan_name, str("ch2"))) {
+        return &state->channels[1];
+    }
 
     return NULL;
 }
 
 fixedpt* resolve_float_field(chan_t* chan, uint8_t* field_name) {
-    if (string_eq(field_name, str("gamma_a"))) {
-        return &chan->gamma_a;
-    }
-    if (string_eq(field_name, str("gamma_b"))) {
-        return &chan->gamma_b;
+    if (string_eq(field_name, str("gamma"))) {
+        return &chan->gamma;
     }
     if (string_eq(field_name, str("val"))) {
         return &chan->val;
-    }
-    if (string_eq(field_name, str("ratio"))) {
-        return &chan->ratio;
     }
     if (string_eq(field_name, str("fade_speed"))) {
         return &chan->fade_speed;
@@ -76,12 +63,10 @@ fixedpt* resolve_float_field(chan_t* chan, uint8_t* field_name) {
 
 void validate_float_field(fixedpt* field, chan_t* chan) {
     switch ((size_t)field - (size_t)chan) {
-        case offsetof(chan_t, gamma_a):
-        case offsetof(chan_t, gamma_b):
+        case offsetof(chan_t, gamma):
             clamp(field, fixedpt_rconst(1.0), fixedpt_rconst(16.0));
             break;
         case offsetof(chan_t, val):
-        case offsetof(chan_t, ratio):
             clamp(field, fixedpt_rconst(0.0), fixedpt_rconst(1.0));
             break;
         case offsetof(chan_t, fade_speed):
@@ -195,27 +180,13 @@ void bump_val(fixedpt* val, fixedpt speed, fixedpt target) {
     }
 }
 
-void pwm_set_chan(size_t index, uint32_t duty_a, uint32_t duty_b) {
-    switch (index) {
-        case 0:
-            pwm_set(0, duty_a);
-            pwm_set(1, duty_b);
-            break;
-    }
-}
-
 void update_channel(size_t chan_index, chan_t* chan) {
-    fixedpt target_a = fixedpt_mul(chan->val, chan->ratio);
-    fixedpt target_b = fixedpt_mul(chan->val, (fixedpt_rconst(1.0) - chan->ratio));
-    bump_val(&chan->actual_a, chan->fade_speed, target_a);
-    bump_val(&chan->actual_b, chan->fade_speed, target_b);
+    bump_val(&chan->actual, chan->fade_speed, chan->val);
 
-    chan->linear_a = fixedpt_pow(chan->actual_a, chan->gamma_a);
-    chan->linear_b = fixedpt_pow(chan->actual_b, chan->gamma_b);
+    chan->linear = fixedpt_pow(chan->actual, chan->gamma);
+    chan->duty = scale_int(PWM_PERIOD, chan->linear);
 
-    chan->duty_a = scale_int(PWM_PERIOD, chan->linear_a);
-    chan->duty_b = scale_int(PWM_PERIOD, chan->linear_b);
-    pwm_set_chan(chan_index, chan->duty_a, chan->duty_b);
+    pwm_set(chan_index, chan->duty);
 }
 
 void update(state_t* state) {
@@ -231,36 +202,24 @@ void debug_info(state_t* state) {
         uart_queue(str_uint(i + 1));
         uart_queue(str("\r\n"));
         uart_queue(str("  gamma: "));
-        uart_queue(str_fixedpt(state->channels[i].gamma_a));
-        uart_queue(str(", "));
-        uart_queue(str_fixedpt(state->channels[i].gamma_b));
+        uart_queue(str_fixedpt(state->channels[i].gamma));
         uart_queue(str("\r\n"));
         uart_queue(str("  val: "));
         uart_queue(str_fixedpt(state->channels[i].val));
-        uart_queue(str("\r\n"));
-        uart_queue(str("  ratio: "));
-        uart_queue(str_fixedpt(state->channels[i].ratio));
         uart_queue(str("\r\n"));
         uart_queue(str("  fade speed: "));
         uart_queue(str_fixedpt(state->channels[i].fade_speed));
         uart_queue(str("\r\n"));
         uart_queue(str("\r\n"));
         uart_queue(str("  actual: "));
-        uart_queue(str_fixedpt(state->channels[i].actual_a));
-        uart_queue(str(", "));
-        uart_queue(str_fixedpt(state->channels[i].actual_b));
+        uart_queue(str_fixedpt(state->channels[i].actual));
         uart_queue(str("\r\n"));
         uart_queue(str("  linear: "));
-        uart_queue(str_fixedpt(state->channels[i].linear_a));
-        uart_queue(str(", "));
-        uart_queue(str_fixedpt(state->channels[i].linear_b));
+        uart_queue(str_fixedpt(state->channels[i].linear));
         uart_queue(str("\r\n"));
         uart_queue(str("  duty: "));
-        uart_queue(str_uint(state->channels[i].duty_a));
-        uart_queue(str(", "));
-        uart_queue(str_uint(state->channels[i].duty_b));
-        uart_queue(str("\r\n"));
-        uart_queue(str("  duty max: "));
+        uart_queue(str_uint(state->channels[i].duty));
+        uart_queue(str(" / "));
         uart_queue(str_uint(PWM_PERIOD));
         uart_queue(str("\r\n"));
     }
